9a286ec615d2632034364e6870cb747078f51b87
Mateusz Toczyski <mtoczyski303@gmail.com>
2026-06-20 08:43:21 +0200

    add: Amount and Contains conditions

---
+++ sage/domain/conditions.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from decimal import Decimal
+from typing import Callable, override
+
+from sage.domain.transaction import Transaction
+
+
+class Condition(ABC):
+    @abstractmethod
+    def evaluate(self, tx: Transaction) -> bool: ...
+
+
+class Contains(Condition):
+    def __init__(self, text: str) -> None:
+        super().__init__()
+        self.text: str = text
+
+    @override
+    def evaluate(self, tx: Transaction) -> bool:
+        return self.text in tx.description
+
+
+class Amount(Condition):
+    def __init__(
+        self, op: Callable[[Decimal, Decimal], bool], threshold: Decimal
+    ) -> None:
+        super().__init__()
+        self.op: Callable[[Decimal, Decimal], bool] = op
+        self.threshold: Decimal = threshold
+
+    @override
+    def evaluate(self, tx: Transaction) -> bool:
+        return self.op(tx.amount, self.threshold)
